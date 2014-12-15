3561d82276e5cc4fcd0d23308426c1496cda8c07
Arunram Kalaiselvan <arunram@placeiq.com>
2014-12-15 01:00:37 -0500

    Updated google calendar service to conform to our data set and create events

--- api/services/GCalService.js
+++ api/services/GCalService.js
@@ -1,50 +1,37 @@
-exports.createEvent = function(options) {
+var gcal = require('google-calendar');
+exports.createEvent = function(options, callback) {
   var addEventBody = {
-    'status':'confirmed',
-    'summary': request.body.contact.firstName + ' ' + request.body.contact.lastName,
-    'description': request.body.contact.phone + '\n' + request.body.contact.details,
-    'organizer': {
-      'email': googleUserId,
+    status:'confirmed',
+    summary: options.title,
+    description: options.description,
+    organizer: {
+      'email': options.organizer.email,
       'self': true
     },
-    'start': {
-      'dateTime': request.body.startdate,
+    start: {
+      'dateTime': options.start
     },
-    'end': {
-      'dateTime': request.body.enddate
+    end: {
+      'dateTime': options.end
     },
-    'attendees': [
+    attendees: [
         {
-          'email': googleUserId,
+          'email': options.organizer.email,
           'organizer': true,
           'self': true,
           'responseStatus': 'needsAction'
-        },
-        {
-          'email': request.body.contact.email,
-        'organizer': false,
-        'responseStatus': 'needsAction'
         }
     ]
   };
 
-  var addGoogleEvent = function(accessToken){
-    //instantiate google calendar instance
-    var google_calendar = new gcal.GoogleCalendar(accessToken);
-    google_calendar.events.insert(googleUserId, addEventBody, function(addEventError, addEventResponse){
-      console.log('GOOGLE RESPONSE:', addEventError, addEventResponse);
-
-      if(!addEventError)
-        response.send(200, addEventResponse);
-
-      response.send(400, addEventError);
+  for (var i=0; i < options.attendees; i++) {
+    addEventBody.attendees.push({
+      'email' : options.attendees[i].email,
+      'organizer' : false,
+      'responseStatus' : 'needsAction'
     });
-  };
+  }
 
-  //retrieve current access token
-  getAccessToken().then(function(accessToken){
-    addGoogleEvent(accessToken);
-  }, function(error){
-    //TODO: handle error
-  });
+  var google_calendar = new gcal.GoogleCalendar(options.accessToken);
+  google_calendar.events.insert(options.organizer.email, addEventBody, callback);
 }
